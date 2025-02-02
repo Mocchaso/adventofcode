b77c4693584648a7351083edd605ca625a2958f3
Koki Sakamoto <saka.ksftn2718@gmail.com>
2025-02-02 17:39:50 +0900

    2023 day07: main.pyの実行処理を関数化、実行速度も計測するように

--- Python/2023/day07/main.py
+++ Python/2023/day07/main.py
@@ -1,16 +1,28 @@
+import sys
+import time
+
 from hand_list_parser import HandListParser
 from hand_evaluator import *
 from hand_ranker import *
+from common.time_util import getFormattedElapsedTimeInfo
 
 
-if __name__ == "__main__":
-    hand_list = HandListParser.parse("./hand_list_example.txt")
-    # hand_list = HandListParser.parse("./hand_list_question.txt")
+def execute(hand_list_text_path_str: str, question: int) -> None:
+    start_time = time.perf_counter()
+
+    hand_list = HandListParser.parse(hand_list_text_path_str)
     print()
 
-    hand_evaluator = HandEvaluatorImpl2()
-    hand_ranker = HandRankerImpl2(hand_evaluator)
-
+    if question == 1:
+        hand_evaluator = HandEvaluatorImpl()
+        hand_ranker = HandRankerImpl(hand_evaluator)
+    elif question == 2:
+        hand_evaluator = HandEvaluatorImpl2()
+        hand_ranker = HandRankerImpl2(hand_evaluator)
+    else:
+        print(f"Unsupported question: {question}")
+        sys.exit()
+    
     sorted_hand_list = hand_ranker.rank_hands(hand_list)
     total_bounty = 0
     for i, fix_hand in enumerate(sorted_hand_list):
@@ -19,3 +31,37 @@ if __name__ == "__main__":
     
     print()
     print(f"total_bounty: {total_bounty}")
+
+    end_time = time.perf_counter()
+
+    print(f"Process time: {getFormattedElapsedTimeInfo(start_time, end_time)}")
+
+
+if __name__ == "__main__":
+    HAND_LIST_EXAMPLE_TEXT_PATH = "./hand_list_example.txt"
+    HAND_LIST_QUESTION_TEXT_PATH = "./hand_list_question.txt"
+
+    # 問題1の例題
+    print("[Part 1 - example]")
+    execute(HAND_LIST_EXAMPLE_TEXT_PATH, 1)
+    print()
+    print("====================================================================================================")
+    print()
+
+    # 問題1の問題
+    print("[Part 1 - question]")
+    execute(HAND_LIST_QUESTION_TEXT_PATH, 1)
+    print()
+    print("====================================================================================================")
+    print()
+
+    # 問題2の例題
+    print("[Part 2 - example]")
+    execute(HAND_LIST_EXAMPLE_TEXT_PATH, 2)
+    print()
+    print("====================================================================================================")
+    print()
+
+    # 問題2の問題
+    print("[Part 2 - question]")
+    execute(HAND_LIST_QUESTION_TEXT_PATH, 2)
